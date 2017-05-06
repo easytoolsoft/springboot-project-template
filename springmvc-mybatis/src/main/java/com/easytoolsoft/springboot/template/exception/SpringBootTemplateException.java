8fbafdd2f095d441ec41abc840bd0b1ad09ee14c
xianrendzw <14068728@qq.com>
2017-05-07 00:34:32 +0800

    重构项目结构

--- springmvc-mybatis/src/main/java/com/easytoolsoft/springboot/template/exception/SpringBootTemplateException.java
+++
@@ -1,8 +0,0 @@
-package com.easytoolsoft.springboot.template.exception;
-
-/**
- * @author zhiwei.deng
- * @date 2017-04-19
- **/
-public class SpringBootTemplateException extends RuntimeException {
-}
