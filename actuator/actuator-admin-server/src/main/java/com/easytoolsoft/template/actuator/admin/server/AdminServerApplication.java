053d79d7dfa00ae8922ba5c63fc70b9ab1dd0d41
xianrendzw <14068728@qq.com>
2017-05-07 17:06:05 +0800

    重构项目结构完成

---
+++ actuator/actuator-admin-server/src/main/java/com/easytoolsoft/template/actuator/admin/server/AdminServerApplication.java
@@ -0,0 +1,14 @@
+package com.easytoolsoft.template.actuator.admin.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+}
